ccf9bfc11b2b7ba14815423fe6dabcfecc87054b
Wajeeha <wajeehamushtaq1@gmial.com>
2020-09-26 19:39:23 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector('.talk');
+const content = document.querySelector('.content');
+const SpeechRecognition = window.speechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition(); 
+
+recognition.onstart = function() {
+   console.log('Voice Is Activated, You Can Speak');
+};
+
+recognition.onresult = function(event) {
+   const current = event.resultIndex;
+   const transcript = event.results[current][0].transcript;
+   content.textContent=transcript;
+   readOutLoud(transcript);
+};
+
+btn.addEventListener('click', () => {
+   recognition.start();
+});
+
+function readOutLoud(message){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text=message;
+    speech.volume=1;
+    speech.rate=1;
+    speech.pitch=1;
+
+    window.speechSynthesis.speak(speech);
+}
